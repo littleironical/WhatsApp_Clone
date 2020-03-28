6d45573c559e4fa7b954d41ac68aedb3561bba42
Hardik kumar <61787056+Hardikkr@users.noreply.github.com>
2020-03-28 19:23:03 +0530

    Edit in ChatsScreen

--- lib/ChatsScreen.dart
+++ lib/ChatsScreen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:projectxi/DummyData.dart';
+
+Widget spaceBetween(){
+  return Divider(height: 5.0, indent: 80.0, endIndent: 17.0);
+}
 
 class ChatsScreen extends StatefulWidget {
   @override
@@ -6,53 +11,104 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
-
-List<int> items = List();
-
-@override
-  void initState() {
-    for(int i=0; i<20; i++){
-      items.add(i);
-    }
-    super.initState();
-  }
-
     @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        accentColor: Color(0xFF075E54),
+      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          body:  ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (BuildContext context, int index){
-              return mylist();
-            }
+          body:  ListView(
+            padding: EdgeInsets.only(bottom: 80.0, top: 0.0),
+            addRepaintBoundaries: true,
+             children: <Widget>[
+               DummyData(
+                 name: 'Hardik Kumar',
+                 message: 'Flutter is so cool',
+                 time: '5:25',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Jeff Bezoz',
+                 message: 'I\'m the CEO of Google',
+                 time: '13:09',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'John Cena',
+                 message: 'You can\'t see me',
+                 time: '13:34',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Narendra Modi',
+                 message: 'Don\'t go outside',
+                 time: '10:59',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Akshay Kumar',
+                 message: 'Hey buddy...how are you',
+                 time: '14:16',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Ravina Tandan',
+                 message: 'Wash your hands properly',
+                 time: '9:30',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Roman Reigns',
+                 message: 'I will win tonight\'s fight',
+                 time: '8:00',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Bhuvan Bam',
+                 message: 'I\'m going to United States tommorrow',
+                 time: '11:47',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Varun Dhavan',
+                 message: 'Happy Quarantine buddy',
+                 time: '5:20',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Ashish Chanchlani',
+                 message: 'I\'m going tto release my new video today',
+                 time: '6:31',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Carry Minati',
+                 message: 'Take my pc bro. I have a new one',
+                 time: '10:10',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Tanmay Bhatt',
+                 message: 'Where are you? I\'m waiting outside',
+                 time: '10:36',
+               ),
+               spaceBetween(),
+               DummyData(
+                 name: 'Debbo Ratnani',
+                 message: 'Can I take your pics?',
+                 time: '20:13',
+               ),
+             ],
           ),
+
           floatingActionButton: FloatingActionButton(
           onPressed: () {},
           backgroundColor: Color(0xFF25D366),
-          child: Icon(Icons.message),
-         ),
-      )
-    );
-  }
-}
-
-Widget mylist(){
-  return  ListTile(
-    leading: CircleAvatar(
-      radius: 26.0,
-      child: Text('H',
-        style: TextStyle(
-          color: Colors.lightGreen[800],
-          fontWeight: FontWeight.w900,
+          child: Icon(Icons.message)
+         )
         ),
-      ),
-      backgroundColor: Colors.grey,
-    ),
-    title: Text('Hardik Kumar'),
-    subtitle: Text('Hey Buddy! How are you?'),
-    trailing: Text('22:13'),
-    onTap: () {},
-  );
+      );
+  }
 }
