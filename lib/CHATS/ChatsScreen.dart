371c34062cc162d9e89742901c1bdc942ca5176f
Hardik kumar <61787056+Hardikkr@users.noreply.github.com>
2020-03-30 19:21:52 +0530

    added images in chats screen

--- lib/CHATS/ChatsScreen.dart
+++ lib/CHATS/ChatsScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projectxi/main.dart';
 
 import 'DummyData.dart';
 
@@ -22,84 +23,96 @@ class _ChatsScreenState extends State<ChatsScreen> {
       home: Scaffold(
           body:  ListView(
             padding: EdgeInsets.only(bottom: 80.0, top: 0.0),
-            addRepaintBoundaries: true,
              children: <Widget>[
                DummyData(
                  name: 'Hardik Kumar',
                  message: 'Flutter is so cool',
                  time: '22:55',
+                 netimage: hardikkumar,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Sundar Pichai',
                  message: 'I\'m the CEO of Google',
                  time: '18:25',
+                 netimage: sundarpichai,
                ),
                spaceBetween(),
                DummyData(
                  name: 'John Cena',
                  message: 'You can\'t see me',
                  time: '14:20',
+                 netimage: johncena,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Narendra Modi',
                  message: 'Don\'t go outside',
                  time: '09:45',
+                 netimage: narendramodi,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Akshay Kumar',
                  message: 'Hey buddy...how are you',
                  time: '09:16',
+                 netimage: akshaykumar,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Ravina Tandan',
                  message: 'Wash your hands properly',
                  time: 'Yesterday',
+                 netimage: ravinatandon,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Roman Reigns',
                  message: 'I will win tonight\'s fight',
                  time: 'Yesterday',
+                 netimage: romanreigns,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Bhuvan Bam',
                  message: 'I\'m going to United States tommorrow',
                  time: '3/26/20',
+                 netimage: bhuvanbam,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Varun Dhavan',
                  message: 'Happy Quarantine buddy',
                  time: '3/24/20',
+                 netimage: varundhavan,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Ashish Chanchlani',
-                 message: 'I\'m going tto release my new video today',
+                 message: 'I\'ll release my new video today',
                  time: '3/20/20',
+                 netimage: ashishchanchlani,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Carry Minati',
                  message: 'Take my pc bro. I have a new one',
                  time: '3/19/20',
+                 netimage: carryminati,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Tanmay Bhatt',
                  message: 'Where are you? I\'m waiting outside',
                  time: '3/7/20',
+                 netimage: tanmaybhatt,
                ),
                spaceBetween(),
                DummyData(
                  name: 'Debbo Ratnani',
                  message: 'Can I take your pics?',
                  time: '3/5/20',
+                 netimage: dabbooratnani,
                ),
              ],
           ),
